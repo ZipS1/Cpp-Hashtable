736ae0a560dcb458cbd855c213c4bd2edffcd4e7
Sergei Tsvetkov <czwetkow.serega@yandex.ru>
2023-09-20 07:54:37 +0300

    Add test methods and read docs ( 20m )

--- HashTableTests/HashTableTests.cpp
+++ HashTableTests/HashTableTests.cpp
@@ -5,12 +5,33 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace HashTableTests
 {
-	TEST_CLASS(HashTableTests)
+	TEST_CLASS(Class)
 	{
 	public:
 		
 		TEST_METHOD(TestMethod1)
 		{
+			Assert::AreEqual(1, 1);
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			Assert::AreEqual(2, 2);
+		}
+	};
+
+	TEST_CLASS(Class2)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::AreEqual(1, 1);
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			Assert::AreEqual(2, 2);
 		}
 	};
 }
